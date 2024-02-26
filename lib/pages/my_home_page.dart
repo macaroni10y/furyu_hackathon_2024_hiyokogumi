47f187ef024d20f649bf506e3743a23667422c30
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-02-26 22:10:10 +0900

    introduce firebase

---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Future<void> _loginAnonymously() async {
+    FirebaseAuth.instance.signInAnonymously();
+  }
+
+  void _sendTestMessage() {
+    debugPrint('Sending message to Firestore');
+    FirebaseFirestore.instance.collection('test_collection').add({
+      'id': FirebaseAuth.instance.currentUser?.uid,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loginAnonymously();
+    _sendTestMessage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+                width: 160,
+                height: 160,
+                child: Image.asset('assets/images/animal_mark_hiyoko.png')),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
