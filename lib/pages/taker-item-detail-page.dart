cbfdbb56eaacf5d5ffcf68d866f73e31ee5efbcb
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-03-02 12:16:50 +0900

    詳細画面のデザイン

--- lib/pages/taker-item-detail-page.dart
+++ lib/pages/taker-item-detail-page.dart
@@ -58,86 +58,111 @@ class _TakerItemDetailPageState extends State<TakerItemDetailPage> {
 
   /// 他人が出品した商品の詳細ページのbodyを生成する
   Widget _buildBody(Item item) {
-    return SingleChildScrollView(
-        child: Column(
-      children: [
-        Container(
-          width: 300,
-          height: 300,
-          margin: const EdgeInsets.fromLTRB(16, 80, 16, 24),
-          child: Image.network(item.imageUrl, fit: BoxFit.contain),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/detail/外枠.png"),
+          fit: BoxFit.cover,
         ),
-        Container(
-          margin: EdgeInsets.fromLTRB(16, 0, 16, 24),
-          child: Row(
-            children: [
-              Spacer(
-                flex: 1,
+      ),
+      child: SafeArea(
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: Image.network(
+                item.imageUrl,
+                fit: BoxFit.cover,
+                width: 300,
+                height: 200,
               ),
-              // ここのSizedBoxはボタンの幅とボタンとの間隔分のサイズで間隔を入れている
-              SizedBox(
-                width: 24,
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: 256,
-                ),
-                child: Text(
-                  // TODO: font sizeを文字数に合わせて可変にしないと、改行が入ってレイアウト崩れる
-                  item.title,
-                  style: TextStyle(
-                    fontSize: 30,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 24),
+              child: Row(
+                children: [
+                  Spacer(
+                    flex: 1,
                   ),
+                  // ここのSizedBoxはボタンの幅とボタンとの間隔分のサイズで間隔を入れている
+                  SizedBox(
+                    width: 24,
+                  ),
+                  Container(
+                    child: Text(
+                      item.title,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _like();
+                        favorite = true;
+                      });
+                    },
+                    icon: Icon(Icons.favorite),
+                    color: favorite ? Colors.pink : Colors.black12,
+                    highlightColor: Colors.white,
+                  ),
+                  // いいねの数
+                  Text('$_favoriteCount'),
+                  Spacer(
+                    flex: 1,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 2.5),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                item.description,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
                 ),
               ),
-              SizedBox(
-                width: 8,
-              ),
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    _like();
-                    favorite = true;
-                  });
-                },
-                icon: Icon(Icons.favorite),
-                color: favorite ? Colors.pink : Colors.black12,
-                highlightColor: Colors.white,
-              ),
-              // いいねの数
-              Text('$_favoriteCount'),
-              Spacer(
-                flex: 1,
-              )
-            ],
-          ),
-        ),
-        Text(
-          item.description,
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
+            ),
 
-        // TODO: タグを表示する部分を入れる
+            // TODO: タグを表示する部分を入れる
 
-        Container(
-          margin: const EdgeInsets.fromLTRB(16, 32, 16, 32),
-          child: CupertinoButton(
-            onPressed: () => {
-              Navigator.push(
-                  context,
-                  CupertinoPageRoute(
-                      builder: (context) => ChatPage(
-                            item: widget.item,
-                            candidateId: FirebaseAuth.instance.currentUser!.uid,
-                          )))
-            },
-            child: Text("やりとりする"),
-          ),
-        ),
-      ],
-    ));
+            Container(
+              margin: const EdgeInsets.fromLTRB(16, 32, 16, 32),
+              child: CupertinoButton(
+                  onPressed: () => {
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => ChatPage(
+                                      item: widget.item,
+                                      candidateId: FirebaseAuth
+                                          .instance.currentUser!.uid,
+                                    )))
+                      },
+                  child: Image.asset(
+                    "assets/images/detailfromother/コメント(3件)を表示….png",
+                    width: 150,
+                  )),
+            ),
+            SizedBox(
+              height: 140,
+            ),
+          ],
+        )),
+      ),
+    );
   }
 
   @override
